f81adccec2c55231d3560d0498ebbc6fbcf53638
HSZJS <heshuzhan@heshuzhandeMacBook-Pro.local>
2016-07-26 16:21:45 +0800

    2.Objective-C Data Types
    数据类型

---
+++ iOSReady1/DataType.h
@@ -0,0 +1,32 @@
+//
+//  DataType.h
+//  iOSReady1
+//
+//  Created by 何舒展 on 16/7/26.
+//  Copyright © 2016年 何舒展. All rights reserved.
+//
+
+#ifndef DataType_h
+#define DataType_h
+
+/*
+  Data Types
+            
+ BOOL: Type to represent a Boolean value.
+ Catagory: an opaque type that represetns a category
+ class: an opaque type that represents an Object-C class.
+ Ivar :an opaque type that represents an instance variable.
+ Method :An opaque type that represents a method in a class definition.
+ NSInteger: Used to describe an integer.
+ NSUInteger: Used to describe an unsigned integer.
+ SEL: defines an opaque type that represents a method selector.
+ STR:
+ arith_t
+ objc_method_description:defines an objectvie_c method.
+ objc_method_description_list
+ objc_propery_t: an opaque type that represents an objective-c declared property.
+ objc_protocol_list:represents a list of formal protocols.
+ uarith_t
+ 
+ */
+#endif /* DataType_h */
