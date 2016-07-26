550b4f04fca1c65809a040758080894442781c79
HSZJS <heshuzhan@heshuzhandeMacBook-Pro.local>
2016-07-26 16:34:32 +0800

    object-c Overview
    overview

---
+++ Overview.h
@@ -0,0 +1,20 @@
+//
+//  Overview.h
+//  iOSReady1
+//
+//  Created by 何舒展 on 16/7/26.
+//  Copyright © 2016年 何舒展. All rights reserved.
+//
+
+#ifndef Overview_h
+#define Overview_h
+/*
+    The Objective-C module APIs define the base of the Objective-C language. These APIs include:
+   Type such as the NSObject class and the NSObject protocol that provide the root functionality of most Objective-C classes
+    
+Functions and data structures that comprise the Ovjective-C runtime,which provides support for the dynamic properties of the Ovjective-C
+ language
+ You typically don't need to use this module directly.
+ */
+
+#endif /* Overview_h */
